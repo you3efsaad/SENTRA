8fe34d9872cf8b2963c2dfa25763c9fff65e9712
you3efsaad <youseft.k12@gmail.com>
2026-06-20 07:48:23 +0300

    add load forecasting

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 from app import create_app
+import subprocess
+
 
 app = create_app()
 app.secret_key = "any_random_secret_string_here" 
 
 if __name__ == "__main__":
+    subprocess.Popen(["python", "ai_engine/aggregator_service.py"])
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
